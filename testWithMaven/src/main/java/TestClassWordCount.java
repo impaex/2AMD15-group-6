9188fbcee5276e1125bab7fb4d6abeca49f924ec
Max <43404248+impaex@users.noreply.github.com>
2025-02-22 16:46:25 +0100

    feat: Q1 implemented

--- testWithMaven/src/main/java/TestClassWordCount.java
+++ testWithMaven/src/main/java/TestClassWordCount.java
@@ -3,6 +3,11 @@ import scala.Tuple2;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Dataset;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,16 +23,34 @@ public final class TestClassWordCount {
             System.exit(1);
         }
 
-
+        
         SparkSession spark = SparkSession
                 .builder()
                 .appName("JavaWordCount")
                 .master("local[*]")
                 .getOrCreate();
 
+        /* 
+         *  Question 1: Loading the data
+         */
+
+        // (B): RDD
         JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
 
         long lineCount = lines.count();
+
+        // (A): Dataset
+
+        // We define a schema so that Spark doesn't dynamically tries to interpret the data upon loading, reducing overhead
+        StructType schema = new StructType(new StructField[]{
+            DataTypes.createStructField("userId", DataTypes.IntegerType, false),
+            DataTypes.createStructField("songid", DataTypes.IntegerType, false),
+            DataTypes.createStructField("rating", DataTypes.IntegerType, true),
+        });
+
+        Dataset<Row> dataSet = spark.read().schema(schema).csv(args[0]);
+
+        long rowCount = dataSet.count();
 
         // JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator()).filter(s->s.startsWith("wi"));
 
@@ -40,7 +63,8 @@ public final class TestClassWordCount {
         //     System.out.println(tuple._1() + ": " + tuple._2());
         // }
 
-        System.out.println("Total number of lines: " + lineCount);
+        System.out.println("Total number of lines in RDD: " + lineCount);
+        System.out.println("Total number of rows in dataset: " + rowCount);
 
         spark.stop();
     }
